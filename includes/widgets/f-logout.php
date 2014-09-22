64aae0d2da659efd83b1d2d119d70317f716a69b
Ankur Sinha <ankur218@yahoo.com>
2014-09-22 20:06:53 +0530

    Updated all the stylesheets and scripts

--- includes/widgets/f-logout.php
+++ includes/widgets/f-logout.php
@@ -3,6 +3,7 @@
               <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download">Hi, <?php echo $f_data['f_fname']; ?><span class="caret"></span></a>
               <ul class="dropdown-menu" aria-labelledby="download">
                 <li><a href="bookings.php">Bookings</a></li>
+                <li><a href="changepass.php">Change Password</a></li>
                 <li><a href="logout.php">Logout</a></li>
               </ul>
             </li>
